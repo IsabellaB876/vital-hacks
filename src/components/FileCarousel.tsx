6a24f1e00d1d15a55dc4de1f96b018d078af38bc
Isabella Borda <bordai876@gmail.com>
2025-03-29 14:22:29 -0400

    update main page and carousel

--- src/components/FileCarousel.tsx
+++ src/components/FileCarousel.tsx
@@ -1,8 +1,14 @@
 import { useState } from "react";
-import { Carousel, Stack } from "react-bootstrap";
+import { Carousel, Stack, Container } from "react-bootstrap";
 import FileCard from "./FileCard";
 
-function FileCarousel() {
+// Define the type of props
+interface CustomCarouselProps {
+  text: string;
+  bgColor: string;
+}
+
+function FileCarousel({ text, bgColor }: CustomCarouselProps) {
   const [index, setIndex] = useState(0);
 
   const handleSelect = (selectedIndex: number) => {
@@ -10,29 +16,51 @@ function FileCarousel() {
   };
 
   return (
-    <Carousel activeIndex={index} onSelect={handleSelect} data-bs-theme="dark">
-      <Carousel.Item>
-        <Stack direction="horizontal" gap={3}>
-          <FileCard />
-          <FileCard />
-          <FileCard />
-        </Stack>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Stack direction="horizontal" gap={3}>
-          <FileCard />
-          <FileCard />
-          <FileCard />
-        </Stack>
-      </Carousel.Item>
-      <Carousel.Item>
-        <Stack direction="horizontal" gap={3}>
-          <FileCard />
-          <FileCard />
-          <FileCard />
-        </Stack>
-      </Carousel.Item>
-    </Carousel>
+    <Stack gap={3} style={{ backgroundColor: bgColor }}>
+      <h3>{text}</h3>
+      <Carousel
+        activeIndex={index}
+        onSelect={handleSelect}
+        data-bs-theme="dark"
+        interval={null}
+        controls={true}
+        indicators={true}
+      >
+        <Carousel.Item>
+          <Stack
+            className="carousel-inner col-10 mx-auto"
+            direction="horizontal"
+            gap={3}
+          >
+            <FileCard />
+            <FileCard />
+            <FileCard />
+          </Stack>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Stack
+            className="carousel-inner col-10 mx-auto"
+            direction="horizontal"
+            gap={3}
+          >
+            <FileCard />
+            <FileCard />
+            <FileCard />
+          </Stack>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Stack
+            className="carousel-inner col-10 mx-auto"
+            direction="horizontal"
+            gap={3}
+          >
+            <FileCard />
+            <FileCard />
+            <FileCard />
+          </Stack>
+        </Carousel.Item>
+      </Carousel>
+    </Stack>
   );
 }
 
