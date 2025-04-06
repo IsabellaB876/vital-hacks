b5e75409278cf1ff76a7d42ae625d94b7250ece6
athejaswi <aarushi.s.t1@gmail.com>
2025-04-05 22:12:02 -0400

    added doctor sign-in + page

---
+++ src/components/DoctorHome.tsx
@@ -0,0 +1,20 @@
+import { Stack } from "react-bootstrap";  
+import FileCarousel from "./FileCarousel";
+import NavBar from "./NavBar";
+
+function DoctorHome(){
+  console.log("hello");
+    return(
+    <Stack gap={3} className="text-start">
+    <NavBar />
+    <h2>Welcome Doctor!</h2>
+    <FileCarousel text="Requested Documents" bgColor="#9EBDF8" />
+    <FileCarousel text="Patient Intake Forms" bgColor="#FFFFFF" />
+    <FileCarousel text="HIPAA and Consent" bgColor="#FFFFFF" />
+    <FileCarousel text="Treatments and Prescriptions" bgColor="#FFFFFF" />
+    <FileCarousel text="Insurance" bgColor="#FFFFFF" />
+  </Stack>
+  );
+}
+
+export default DoctorHome;
