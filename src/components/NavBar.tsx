fc8d86685d9913d6a74cdb6414744af244f47a89
roslyn-maloney <roselily1604@gmail.com>
2025-04-24 22:02:09 -0400

    homepage

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,10 @@
 import { Container, Button, Image } from "react-bootstrap";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import logo from "../assets/react.svg";
+import profile from "../assets/react.svg";
+import logo from "../assets/logo.svg";
+import hammy from "../assets/menu.svg";
+import uploadIcon from "../assets/UploadIcon.svg";
 import UploadScreen from "./UploadScreen";
 import { useState } from "react";
 
@@ -12,20 +15,22 @@ function NavBar() {
   return (
     <>
       <Navbar bg="light" data-bs-theme="light" fixed="top">
-        <Container>
-          <Navbar.Brand href="/">
-            <img src={""} alt="Logo" />
-          </Navbar.Brand>
-          <Nav className="justify-content-end">
-            <Button onClick={toggleDisplay} className="mb-2">
-              Upload
+        <Container className="d-flex justify-content-between align-items-center">
+          <div className="d-flex align-items-center gap-2">
+            <Image className="hammy" src={hammy} alt="hammy" /> <br/>
+            <Navbar.Brand href="/" className="m-0">
+              <Image className="logo" src={logo} alt="Logo"/> MedVault
+            </Navbar.Brand>
+          </div>
+          <Nav className="justify-content-end align-items-center">
+            <Button onClick={toggleDisplay} className="upload-btn mb-2" size="lg">
+              <Image className="upload" src={uploadIcon} alt="upload"/> Upload
             </Button>
             <Nav.Link href="#contact">Profile Name</Nav.Link>
-            <Image src={logo} alt="Profile Photo" roundedCircle />
+            <Image src={profile} alt="Profile Photo" roundedCircle />
           </Nav>
         </Container>
       </Navbar>
-
       {display && <UploadScreen toggleDisplay={toggleDisplay} />}
     </>
   );
