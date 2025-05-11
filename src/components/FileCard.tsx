8581cff7d7ba9573c0b2eb50b4dff09f50f117ca
Isabella Borda <bordai876@gmail.com>
2025-05-10 20:05:12 -0400

    front end fetch files and filtering

--- src/components/FileCard.tsx
+++ src/components/FileCard.tsx
@@ -1,21 +1,23 @@
 import { Button, Card } from "react-bootstrap";
-import icon from "../assets/UploadIcon.svg"
+import icon from "../assets/UploadIcon.svg";
 
 // Define the type of props
 interface FileCardProps {
   hasFile: boolean;
+  title: string;
+  date: string;
+  description: string;
 }
 
-function FileCard({ hasFile }: FileCardProps) {
+function FileCard({ hasFile, title, date, description }: FileCardProps) {
   return (
     <Card style={{ width: "18rem" }}>
       <Card.Img variant="top" src={hasFile ? "" : icon} alt="Uploaded file" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>{title}</Card.Title>
         <Card.Text>
-          Doctor's Name <br />
-          Due Date <br />
-          The files description. lorem ipsum dolor.
+          {date} <br />
+          {description}
         </Card.Text>
         {hasFile ? <Button variant="primary">View File</Button> : <></>}
       </Card.Body>
--- src/components/FileCarousel.tsx
+++ src/components/FileCarousel.tsx
@@ -1,6 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Carousel, Stack, Container } from "react-bootstrap";
 import FileCard from "./FileCard";
+import { getUserFiles } from "../Service";
+
+interface File {
+  name: string;
+  file: string;
+  description: string;
+  date: string;
+  type: string;
+  isRequested: boolean;
+  id: string;
+  requestedFor: string;
+  requestBy: string;
+}
 
 // Define the type of props
 interface CustomCarouselProps {
@@ -14,6 +27,28 @@ function FileCarousel({ text, bgColor }: CustomCarouselProps) {
   const handleSelect = (selectedIndex: number) => {
     setIndex(selectedIndex);
   };
+
+  const [files, setFiles] = useState<any[]>([]);
+
+  useEffect(() => {
+    try {
+      async function loadFiles() {
+        const username = "jimbob";
+        const filesData = await getUserFiles(username);
+        console.log(filesData);
+        setFiles(filesData);
+        console.log("FILTERED FILES:", filteredFiles);
+      }
+      loadFiles();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  const filteredFiles =
+    text === "Requested Documents"
+      ? files.filter((file) => file.isRequested)
+      : files.filter((file) => file.type === text);
 
   return (
     <Stack
@@ -30,27 +65,16 @@ function FileCarousel({ text, bgColor }: CustomCarouselProps) {
         controls={true}
         indicators={true}
       >
-        <Carousel.Item>
-          <Stack direction="horizontal" gap={3}>
-            <FileCard hasFile={text === "Requested Documents" ? false : true} />
-            <FileCard hasFile={text === "Requested Documents" ? false : true} />
-            <FileCard hasFile={text === "Requested Documents" ? false : true} />
-          </Stack>
-        </Carousel.Item>
-        <Carousel.Item>
-          <Stack direction="horizontal" gap={3}>
-            <FileCard hasFile={text === "Requested Documents" ? false : true} />
-            <FileCard hasFile={text === "Requested Documents" ? false : true} />
-            <FileCard hasFile={text === "Requested Documents" ? false : true} />
-          </Stack>
-        </Carousel.Item>
-        <Carousel.Item>
-          <Stack direction="horizontal" gap={3}>
-            <FileCard hasFile={text === "Requested Documents" ? false : true} />
-            <FileCard hasFile={text === "Requested Documents" ? false : true} />
-            <FileCard hasFile={text === "Requested Documents" ? false : true} />
-          </Stack>
-        </Carousel.Item>
+        {filteredFiles.map((file) => (
+          <Carousel.Item key={file.unique_id}>
+            <FileCard
+              hasFile={file.isRequested}
+              title={file.file_name}
+              date={file.date}
+              description={file.description}
+            />
+          </Carousel.Item>
+        ))}
       </Carousel>
       {}
     </Stack>
